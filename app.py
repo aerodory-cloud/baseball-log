f5a8d6ab2f4ad3f172bd9faeb61db2cb98f3f1b9
aerodory-cloud <aerodory@gmail.com>
2026-01-28 23:01:50 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -213,7 +213,7 @@ def render_daily_log(username, date_str):
             bad = st.text_area("Bad", value=txt('self_bad'), height=80, placeholder="Bad", label_visibility="collapsed", key=f"bad_{date_str}")
         with fc1:
             st.warning("🗣️ 코치 피드백 (Coach's Feedback)")
-            coach = st.text_area("coach", value=txt('coach_feedback'), height=180, label_visibility="collapsed", key=f"coach_{date_str}")
+            coach = st.text_area("coach", value=txt('coach_feedback'), height=175, label_visibility="collapsed", key=f"coach_{date_str}")
 
         st.markdown("<hr style='margin: 10px 0; border-top: 1px solid #f0f2f6; border-bottom: none;'>", unsafe_allow_html=True)
 
